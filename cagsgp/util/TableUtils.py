b18e1e78b187aba5c0b3efa1483c980bc7f10ed7
lurovi <luigirovito2@gmail.com>
2023-08-16 18:07:43 +0200

    Updated with expl_pipe competitor_rate torus_dim and kruskal-wallis test

--- cagsgp/util/TableUtils.py
+++ cagsgp/util/TableUtils.py
@@ -16,12 +16,14 @@ class TableUtils:
     def print_table_wilcoxon_datasets_cellular_vs_tournament(
             folder_name: str,
             seed_list: list[int],
-            topologies_radius_shapes: list[tuple[str, int, tuple[int, ...]]],
+            torusdim_radius_shapes: list[tuple[int, int, tuple[int, ...]]],
             dataset_names: list[str],
             pop_size: int,
             num_gen: int,
             last_gen: int,
             max_depth: int,
+            expl_pipe: str,
+            competitor_rate: float,
             duplicates_elimination: str,
             crossover_probability: float,
             mutation_probability: float,
@@ -39,9 +41,9 @@ class TableUtils:
         # Load tournament-4 baseline result
         # ===================
 
-        fitness['Tournament-4'] = {}
+        fitness[StringUtils.concat('TD', str(0))+'-'+str(4)] = {}
         for dataset_name in dataset_names:
-            fitness['Tournament-4'][dataset_name] = {'Train': [], 'Test': []}
+            fitness[StringUtils.concat('TD', str(0))+'-'+str(4)][dataset_name] = {'Train': [], 'Test': []}
             for seed in seed_list:
                 d: dict[str, Any] = ResultUtils.read_single_json_file(
                     folder_name=folder_name,
@@ -49,8 +51,10 @@ class TableUtils:
                     pop_size=pop_size,
                     num_gen=num_gen,
                     max_depth=max_depth,
-                    neighbors_topology='tournament',
+                    torus_dim=0,
                     dataset_name=dataset_name,
+                    expl_pipe=expl_pipe,
+                    competitor_rate=competitor_rate,
                     duplicates_elimination=duplicates_elimination,
                     pop_shape=(pop_size,),
                     crossover_probability=crossover_probability,
@@ -62,15 +66,15 @@ class TableUtils:
                     seed=seed
                 )
                 best: dict[str, Any] = d['history'][last_gen]
-                fitness['Tournament-4'][dataset_name]['Train'].append(best['Fitness']['Train RMSE'])
-                fitness['Tournament-4'][dataset_name]['Test'].append(best['Fitness']['Test RMSE'])
+                fitness[StringUtils.concat('TD', str(0))+'-'+str(4)][dataset_name]['Train'].append(best['Fitness']['Train RMSE'])
+                fitness[StringUtils.concat('TD', str(0))+'-'+str(4)][dataset_name]['Test'].append(best['Fitness']['Test RMSE'])
     
         # ===================
         # Load other methods
         # ===================
 
-        for topology, radius, shape in topologies_radius_shapes:
-            current_method: str = StringUtils.only_first_char_upper(topology)+'-'+str(radius)
+        for torus_dim, radius, shape in torusdim_radius_shapes:
+            current_method: str = StringUtils.concat('TD', str(torus_dim))+'-'+str(radius)
             fitness[current_method] = {}
             for dataset_name in dataset_names:
                 fitness[current_method][dataset_name] = {'Train': [], 'Test': []}
@@ -81,8 +85,10 @@ class TableUtils:
                         pop_size=pop_size,
                         num_gen=num_gen,
                         max_depth=max_depth,
-                        neighbors_topology=topology,
+                        torus_dim=torus_dim,
                         dataset_name=dataset_name,
+                        expl_pipe=expl_pipe,
+                        competitor_rate=competitor_rate,
                         duplicates_elimination=duplicates_elimination,
                         pop_shape=shape,
                         crossover_probability=crossover_probability,
@@ -101,14 +107,12 @@ class TableUtils:
         # Print table content
         # ===================
 
-        for method in ['Line-1', 'Line-2', 'Line-3', 'Line-4',
-                        'Matrix-1', 'Matrix-2', 'Matrix-3', 'Matrix-4',
-                        'Cube-1', 'Cube-2', 'Cube-3', 'Cube-4']:
+        for method in ['TD2-1', 'TD2-2', 'TD2-3']:
             tab_str += '{' + method + '}' + '\n'
-            for dataset_name in ['airfoil', 'bioav', 'concrete', 'ppb', 'slump', 'toxicity', 'yacht']:
+            for dataset_name in ['vladislavleva14', 'airfoil', 'keijzer6', 'concrete', 'slump', 'toxicity', 'yacht', 'parkinson']:
                 for split_type in ['Train', 'Test']:
                     a: list[float] = fitness[method][dataset_name][split_type]
-                    b: list[float] = fitness['Tournament-4'][dataset_name][split_type]
+                    b: list[float] = fitness[StringUtils.concat('TD', str(0))+'-'+str(4)][dataset_name][split_type]
                     p: float = round(stats.wilcoxon(a, b, alternative="less").pvalue, 2) if a != b else 1.0
                     tab_str += '& ' + str(p) + ' '
             tab_str += '\n'
@@ -125,12 +129,14 @@ class TableUtils:
             tournament_pressures: list[int],
             bonferroni_correction: bool,
             wilcoxon_only_with_baseline: bool,
-            topologies_radius_shapes: list[tuple[str, int, tuple[int, ...]]],
+            torusdim_radius_shapes: list[tuple[int, int, tuple[int, ...]]],
             dataset_names: list[str],
             pop_size: int,
             num_gen: int,
             last_gen: int,
             max_depth: int,
+            expl_pipe: str,
+            competitor_rate: float,
             duplicates_elimination: str,
             crossover_probability: float,
             mutation_probability: float,
@@ -151,18 +157,20 @@ class TableUtils:
         # ===================
 
         for tournament_pressure in tournament_pressures:
-            fitness['Tournament-'+str(tournament_pressure)] = {}
+            fitness['TD0-'+str(tournament_pressure)] = {}
             for dataset_name in dataset_names:
-                fitness['Tournament-'+str(tournament_pressure)][dataset_name] = []
+                fitness['TD0-'+str(tournament_pressure)][dataset_name] = []
                 for seed in seed_list:
                     d: dict[str, Any] = ResultUtils.read_single_json_file(
-                        folder_name=os.environ['CURRENT_CODEBASE_FOLDER'] + 'python_data/CA-GSGP/' + 'results_1.5_0.4' + '/',
+                        folder_name=folder_name,
                         result_file_type='b',
                         pop_size=pop_size,
                         num_gen=num_gen,
                         max_depth=max_depth,
-                        neighbors_topology='tournament',
+                        torus_dim=0,
                         dataset_name=dataset_name,
+                        expl_pipe=expl_pipe,
+                        competitor_rate=competitor_rate,
                         duplicates_elimination=duplicates_elimination,
                         pop_shape=(pop_size,),
                         crossover_probability=crossover_probability,
@@ -174,14 +182,14 @@ class TableUtils:
                         seed=seed
                     )
                     best: dict[str, Any] = d['history'][last_gen]
-                    fitness['Tournament-'+str(tournament_pressure)][dataset_name].append(best['Fitness'][split_type+' RMSE'])
+                    fitness['TD0-'+str(tournament_pressure)][dataset_name].append(best['Fitness'][split_type+' RMSE'])
     
         # ===================
         # Load other methods
         # ===================
 
-        for topology, radius, shape in topologies_radius_shapes:
-            current_method: str = StringUtils.only_first_char_upper(topology)+'-'+str(radius)
+        for torus_dim, radius, shape in torusdim_radius_shapes:
+            current_method: str = StringUtils.concat('TD', str(torus_dim))+'-'+str(radius)
             fitness[current_method] = {}
             for dataset_name in dataset_names:
                 fitness[current_method][dataset_name] = []
@@ -192,8 +200,10 @@ class TableUtils:
                         pop_size=pop_size,
                         num_gen=num_gen,
                         max_depth=max_depth,
-                        neighbors_topology=topology,
+                        torus_dim=torus_dim,
                         dataset_name=dataset_name,
+                        expl_pipe=expl_pipe,
+                        competitor_rate=competitor_rate,
                         duplicates_elimination=duplicates_elimination,
                         pop_shape=shape,
                         crossover_probability=crossover_probability,
@@ -211,37 +221,36 @@ class TableUtils:
         # Print table content
         # ===================
 
-        all_methods: list[str] = ['Tournament-'+str(tournament_pressure) for tournament_pressure in tournament_pressures]
-        if len(topologies_radius_shapes) > 0:
-            all_methods = all_methods + ['Matrix-1', 'Matrix-2', 'Matrix-3', 'Matrix-4']
+        all_methods: list[str] = ['TD0-'+str(tournament_pressure) for tournament_pressure in tournament_pressures]
+        if len(torusdim_radius_shapes) > 0:
+            all_methods = all_methods + ['TD2-1', 'TD2-2', 'TD2-3']
         
         for method in all_methods:
-            id_letter: str = method[0]
-            radius_letter: str = method[(method.index('-')+1):]
-            name_letter: str = method[:method.index('-')]
-            tab_str += '{' + id_letter + radius_letter + '}' + '\n'
+            tab_str += '{' + method + '}' + '\n'
             
-            for dataset_name in ['airfoil', 'bioav', 'concrete', 'ppb', 'slump', 'toxicity', 'yacht']:
+            for dataset_name in ['vladislavleva14', 'airfoil', 'keijzer6', 'concrete', 'slump', 'toxicity', 'yacht', 'parkinson']:
                 a: list[float] = fitness[method][dataset_name]
-                outperformed_methods: list[int] = []
+                outperformed_methods: list[str] = []
                 all_p_values: list[float] = []
                 is_the_wilcoxon_test_meaningful: bool = False
+                all_results_from_competitor_methods: list[list[float]] = []
+                all_results_from_competitor_methods.append(a)
 
                 for method_2 in [mmm for mmm in all_methods if mmm != method]:
-                    id_letter_2: str = method_2[0]
-                    radius_letter_2: str = method_2[(method_2.index('-')+1):]
-                    name_letter_2: str = method_2[:method_2.index('-')]
-
                     b: list[float] = fitness[method_2][dataset_name]
+                    all_results_from_competitor_methods.append(b)
                     p: float = round(stats.wilcoxon(a, b, alternative="less").pvalue, 2) if a != b else 1.0
 
                     all_p_values.append(p)
                     is_meaningful: bool = p < 0.05
                     if is_meaningful:
-                        outperformed_methods.append(int(radius_letter_2))
-                        if method_2 == 'Tournament-4':
+                        outperformed_methods.append(method_2)
+                        if method_2 == StringUtils.concat('TD', str(0))+'-'+str(4):
                             is_the_wilcoxon_test_meaningful = True
                 
+                kruskal_statistic, kruskal_pvalue = stats.kruskal(*all_results_from_competitor_methods)
+                is_the_kruskal_test_meaningful: bool = kruskal_pvalue < 0.05
+
                 if wilcoxon_only_with_baseline:
                     tab_str += '& ' + str(round(statistics.median(a), 2))
                     if is_the_wilcoxon_test_meaningful:
@@ -252,17 +261,17 @@ class TableUtils:
                 else:
                     if bonferroni_correction:
                         reject_bonferroni, pvals_corrected_bonferroni, _, _ = multipletests(all_p_values, alpha=0.05, method='holm')
-                        is_meaningful_bonferroni: bool = np.sum(reject_bonferroni) == len(all_p_values)
+                        is_the_bonferroni_test_meaningful: bool = np.sum(reject_bonferroni) == len(all_p_values)
                         
                         tab_str += '& ' + ('\\bfseries ' if is_the_wilcoxon_test_meaningful else '') + str(round(statistics.median(a), 2))
-                        if is_meaningful_bonferroni:
+                        if is_the_kruskal_test_meaningful and is_the_bonferroni_test_meaningful:
                             tab_str += '{$^{\\scalebox{0.90}{'
                             tab_str += '\\textbf{'+ '*' +  '},'
                             tab_str = tab_str[:-1]
                             tab_str += '}'+'}$}'
                     
                     else:
-                        if len(outperformed_methods) == len(all_methods) - 1:
+                        if is_the_kruskal_test_meaningful and len(outperformed_methods) == len(all_methods) - 1:
                             tab_str += '& ' + str(round(statistics.median(a), 2))
                             tab_str += '{$^{\\scalebox{0.90}{'
                             tab_str += '\\textbf{'+ '*' +  '},'
@@ -282,10 +291,10 @@ class TableUtils:
 
 
 if __name__ == '__main__':
-    # Datasets: ['airfoil', 'bioav', 'concrete', 'parkinson', 'ppb', 'slump', 'toxicity', 'vladislavleva-14', 'yacht']
-    # Datasets: ['airfoil', 'bioav', 'concrete', 'ppb', 'slump', 'toxicity', 'yacht']
+    # Datasets: ['vladislavleva14', 'airfoil', 'keijzer6', 'concrete', 'slump', 'toxicity', 'yacht', 'parkinson']
+    # Datasets: ['vladislavleva14', 'airfoil', 'keijzer6', 'concrete', 'slump', 'toxicity', 'yacht']
     codebase_folder: str = os.environ['CURRENT_CODEBASE_FOLDER']
-    folder_name: str = codebase_folder + 'python_data/CA-GSGP/' + 'results_1.5_0.6' + '/'
+    folder_name: str = codebase_folder + 'python_data/CA-GSGP/' + 'results_1' + '/'
 
     TableUtils.print_table_wilcoxon_medianrmse_datasets_cellular_vs_tournament_for_single_split_type(folder_name=folder_name,
                                               split_type='Test',
@@ -293,16 +302,19 @@ if __name__ == '__main__':
                                               tournament_pressures=[4],
                                               bonferroni_correction=True,
                                               wilcoxon_only_with_baseline=False,
-                                              #topologies_radius_shapes=[],
-                                              topologies_radius_shapes=[('matrix',1,(10,10)),
-                                                                        ('matrix',2,(10,10)),
-                                                                        ('matrix',3,(10,10)),
-                                                                        ('matrix',4,(10,10))],
-                                              dataset_names=['airfoil', 'bioav', 'concrete', 'ppb', 'slump', 'toxicity', 'yacht'],
+                                              #torusdim_radius_shapes=[],
+                                              torusdim_radius_shapes=[(0,4,(100,)),
+                                                                        (2,1,(10,10)),
+                                                                        (2,2,(10,10)),
+                                                                        (2,3,(10,10))
+                                                                        ],
+                                              dataset_names=['vladislavleva14', 'airfoil', 'keijzer6', 'concrete', 'slump', 'toxicity', 'yacht', 'parkinson'],
                                               pop_size=100,
                                               num_gen=1000,
                                               last_gen=1000,
                                               max_depth=6,
+                                              expl_pipe='crossmut',
+                                              competitor_rate=0.6,
                                               duplicates_elimination='nothing',
                                               crossover_probability=0.90,
                                               mutation_probability=0.50,
